325f092074da8e2acd98c5340d33aff36c8f4358
RachitShahrawat <rachitshahrawat@gmail.com>
2025-09-04 02:53:04 +0530

    setup arcjet

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import arcjet,{ detectBot, shield, slidingWindow } from '@arcjet/next'
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
@@ -5,7 +6,28 @@ const isPublicRoute = createRouteMatcher([
   "/",
 ])
 
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 100,
+    }),
+  ],
+})
+
 export default clerkMiddleware(async (auth, req) => {
+    const decision = await aj.protect(req)// When we call arcjet({...}), it creates a client object (which we've named aj). This aj object contains several methods for interacting with the Arcjet service, and protect() is the most important one.
+
+  if (decision.isDenied()) {
+    return new Response(null, { status: 403 })
+  }
   if (!isPublicRoute(req)) await auth.protect()
 })
 
