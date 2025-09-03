997a88704f435290ba4271fb8e9288cce8e48e01
RachitShahrawat <rachitshahrawat@gmail.com>
2025-09-04 02:27:43 +0530

    setup clerk for authentication

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { SignInButton,UserButton } from "@clerk/nextjs";
+
+
+export default function HomePage(){
+    return (
+      <>
+      <SignInButton/>
+      <UserButton/>
+      </>
+    )
+}
