20cc6ca1b50aac8435c739479b78f2d939416046
RachitShahrawat <rachitshahrawat@gmail.com>
2025-09-10 03:28:26 +0530

    changed information of form

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -105,10 +105,10 @@ function NoJobInfos() {
   return (
     <div className="container my-4 max-w-5xl">
       <h1 className="text-3xl md:text-4xl lg:text-5xl mb-4">
-        Welcome to Landr
+        Welcome to PrepAi
       </h1>
       <p className="text-muted-foreground mb-8">
-        To get started, enter information about the type of job you are wanting
+        To get started, enter information about the type of job you want
         to apply for. This can be specific information copied directly from a
         job listing or general information such as the tech stack you want to
         work in. The more specific you are in the description the closer the
